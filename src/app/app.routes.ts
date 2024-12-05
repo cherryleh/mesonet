12cc2b104c14ab596d15ed2e109b02462a547000
cherryleh <cherryle@hawaii.edu>
2024-12-04 20:09:36 -1000

    Add display data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { StationMapComponent } from './station-map/station-map.component';
 import { StationTableComponent } from './station-table/station-table.component';
+import { DataDisplayComponent } from './data-display/data-display.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Root route displays Home
   { path: 'about', component: AboutComponent }, // About route
   { path: 'station-map', component: StationMapComponent},
   { path: 'station-table', component: StationTableComponent},
+  { path: 'data-display', component: DataDisplayComponent},
   { path: '**', redirectTo: '' } // Wildcard route redirects to Home
 ];
