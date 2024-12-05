61d4fc4b411c43a552a3f6b5b12a4a77b327b79e
cherryleh <cherryle@hawaii.edu>
2024-12-04 14:04:48 -1000

    Add home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Root route displays Home
+  { path: 'about', component: AboutComponent }, // About route
+  { path: '**', redirectTo: '' } // Wildcard route redirects to Home
+];
