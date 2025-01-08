c5dea5d5206458d14c2a0dfb13a9be902b1c9511
cherryleh <cherryle@hawaii.edu>
2025-01-08 09:10:46 -1000

    Update dashboard-chart.service.ts

--- src/app/services/dashboard-chart.service.ts
+++ src/app/services/dashboard-chart.service.ts
@@ -13,8 +13,8 @@ export class DashboardChartService {
 
   getData(id: string,start_date: string): Observable<any> {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${environment.apiToken}`); // Your API token here
-    //const url = `${this.apiUrl}&station_ids=${id}&start_date=${start_date}`;
-    const url = `${this.apiUrl}&station_ids=${id}&start_date=2025-01-01T06:00:00-10:00`;
+    const url = `${this.apiUrl}&station_ids=${id}&start_date=${start_date}`;
+    //const url = `${this.apiUrl}&station_ids=${id}&start_date=2025-01-01T06:00:00-10:00`;
     console.log('API request for dashboard chart: ',url);
     return this.http.get<any>(url, { headers });
   }
