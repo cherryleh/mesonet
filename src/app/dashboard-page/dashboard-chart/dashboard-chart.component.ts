64710b2d51162c92f20b3c9585e5caa3c9c240cb
cherryleh <cherryle@hawaii.edu>
2025-03-14 14:17:55 -1000

    Update dashboard-chart.component.ts

--- src/app/dashboard-page/dashboard-chart/dashboard-chart.component.ts
+++ src/app/dashboard-page/dashboard-chart/dashboard-chart.component.ts
@@ -104,9 +104,10 @@ export class DashboardChartComponent implements OnInit, OnDestroy, AfterViewInit
       labels: {
         format: '{value:%b %e, %l:%M %p}', // "Feb 9, 2:30 PM"
         step: 1,
-        rotation: 315
+        rotation: 0 //315
       },
-      tickInterval: 6 * 3600 * 1000, // 6 hours in milliseconds
+      tickInterval: this.selectedDuration === '3d' || this.selectedDuration === '7d' ? 24 * 3600 * 1000 : 6 * 3600 * 1000, // Adjust interval dynamically (6 hours for 3d/7d, 1 hour otherwise)
+
     },
     yAxis: [
       {
