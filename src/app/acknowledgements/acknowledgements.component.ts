dff224533568003e6d3d4d531edb36f365990df2
cherryleh <cherryle@hawaii.edu>
2025-03-24 15:58:38 -1000

    Acknowledgements draft

--- src/app/acknowledgements/acknowledgements.component.ts
+++ src/app/acknowledgements/acknowledgements.component.ts
@@ -17,4 +17,23 @@ export class AcknowledgementsComponent {
     this.isCollapsed = collapsed;
   }
 
+  acknowledgements = [
+    {
+      title: "Funding Support",
+      content: "This project has been supported by the National Science Foundation (NSF) Award #XXXXXXX and additional funding from the University of Hawaii and local stakeholders."
+    },
+    {
+      title: "Collaborators",
+      content: "We acknowledge the contributions of the Hawaii Data Science Institute, the University of Hawaii at Manoa, and various research institutions that have supported climate data collection and dissemination."
+    },
+    {
+      title: "Data Providers",
+      content: "We sincerely appreciate the efforts of the Hawaii Climate Data Portal team, the National Weather Service, and other agencies that provide continuous climate and meteorological data."
+    },
+    {
+      title: "Funding Support",
+      content: "This project has been supported by the National Science Foundation (NSF) Award #XXXXXXX and additional funding from the University of Hawaii and local stakeholders."
+    }
+  ];
+
 }
