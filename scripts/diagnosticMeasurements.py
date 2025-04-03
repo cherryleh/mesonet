72463b8f66cc9225665a26e4ea1cab7b4c67eff1
cherryleh <cherryle@hawaii.edu>
2025-04-03 11:14:34 -1000

    Add Tair RH differencing scripts

--- scripts/diagnosticMeasurements.py
+++ scripts/diagnosticMeasurements.py
@@ -2,6 +2,7 @@ import requests
 import json
 from datetime import datetime, timedelta
 import os 
+import pandas as pd
 
 API_TOKEN = os.getenv("API_TOKEN")
 
@@ -71,3 +72,33 @@ for variable, measurements in measurements_by_variable.items():
     with open(filename, "w") as json_file:
         json.dump(measurements, json_file, indent=4)
     print(f"Saved {filename}")
+
+var_pairs = [('Tair_1_Avg', 'Tair_2_Avg', 'Tair'), ('RH_1_Avg', 'RH_2_Avg','RH')]
+diff_vars = ["Tair","RH"]
+measurements_by_vardiff = {var: {} for var in diff_vars}
+for station in stations:
+    station_id = station.get("station_id")
+    for var1, var2, var_name in var_pairs:
+        url = f"https://api.hcdp.ikewai.org/mesonet/db/measurements?station_ids=0119&var_ids={var1},{var2}&start_date={start_time_str}"
+
+        response = requests.get(url, headers=header, timeout=10)
+        data = response.json()
+
+        df = pd.DataFrame(data)
+
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+        pivot_df = df.pivot(index='timestamp', columns='variable', values='value').sort_index()
+
+        pivot_df = pivot_df.astype(float)
+
+        pivot_df[f'{var_name}_diff']=pivot_df[f'{var1}']-pivot_df[f'{var2}']
+        diff_mean = pivot_df[f'{var_name}_diff'].mean()
+        measurements_by_vardiff[var_name][station_id] = {
+                        "value": diff_mean
+                    }
+        
+for variable, measurements in measurements_by_vardiff.items():
+    filename = f"{variable}_diff.json"
+    with open(filename, "w") as json_file:
+        json.dump(measurements, json_file, indent=4)
