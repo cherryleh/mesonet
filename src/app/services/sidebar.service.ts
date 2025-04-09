20b0bac850c581e612d49e026fe89941e7d3df81
cherryleh <cherryle@hawaii.edu>
2025-04-08 16:09:39 -1000

    Sidebar collapse fix

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,21 @@
+// sidebar.service.ts
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class SidebarService {
+  private collapsed$ = new BehaviorSubject<boolean>(false);
+  isCollapsed$ = this.collapsed$.asObservable();
+
+  toggle() {
+    this.collapsed$.next(!this.collapsed$.value);
+  }
+
+  setCollapsed(value: boolean) {
+    this.collapsed$.next(value);
+  }
+
+  getCollapsed(): boolean {
+    return this.collapsed$.value;
+  }
+}
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SidebarService } from '../services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,11 +13,17 @@ import { CommonModule } from '@angular/common';
 export class SidebarComponent {
   isCollapsed = false; // Default state
 
+  constructor(private sidebarService: SidebarService) {
+    this.isCollapsed = this.sidebarService.getCollapsed();
+  }
+
   // Emit toggle event to parent
   @Output() toggle = new EventEmitter<boolean>();
 
   toggleSidebar() {
-    this.isCollapsed = !this.isCollapsed; // Toggle local state
-    this.toggle.emit(this.isCollapsed);  // Emit event to parent
+    this.isCollapsed = !this.isCollapsed;
+    this.sidebarService.setCollapsed(this.isCollapsed); // keep service updated
+    this.toggle.emit(this.isCollapsed); // still emit if parent needs it
   }
+
 }
