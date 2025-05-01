88c6e3b6dd6694c8efe3ff62cd9171bef3361784
cherryleh <cherryle@hawaii.edu>
2025-05-01 12:13:03 -1000

    Update data-map.component.ts

--- src/app/data-map/data-map.component.ts
+++ src/app/data-map/data-map.component.ts
@@ -414,11 +414,7 @@ private getColorFromValue(value: number, min: number, max: number): string {
       this.map = L.map('map', {
         center: [20.493410, -158.064388],
         zoom: 8,
-        layers: [
-          L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-            attribution: 'Tiles © Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-          })
-        ],
+        layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
         zoomControl: false 
       });
 
