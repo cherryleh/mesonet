aefefa04f5677a1bccfaf6d6910dace190f396e6
cherryleh <cherryle@hawaii.edu>
2025-12-02 15:44:19 -1000

    added time

--- src/app/error-reporting/error-reporting.component.ts
+++ src/app/error-reporting/error-reporting.component.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ErrorReportService } from '../services/error-report.service';
 import { CommonModule } from '@angular/common';
 
+
 interface StationMeta {
   station_id: string;
   name: string;
@@ -24,14 +25,22 @@ export class ErrorReportingComponent {
   dropdownOpen = false;
   stationSearch = '';
 
-  formData = {
+  formData: {
+    username: string;
+    stationNumber: string;
+    variableId: string[];
+    startDate: string;
+    endDate: string;
+    notes: string;
+  } = {
     username: '',
     stationNumber: '',
-    variableId: '',
+    variableId: [],
     startDate: '',
     endDate: '',
     notes: ''
   };
+
 
   private stationCsvUrl =
     'https://raw.githubusercontent.com/HCDP/loggernet_station_data/refs/heads/db-old/csv_data/stations/station_metadata.csv';
@@ -42,6 +51,28 @@ export class ErrorReportingComponent {
   ) {
     this.loadStations();
   }
+
+  variableList: string[] = [
+    "SWin_1_Avg","SWout_1_Avg","LWin_1_Avg","LWout_1_Avg","SWnet_1_Avg",
+    "LWnet_1_Avg","Rnet_1_Avg","Albedo_1_Avg","Tsrf_1_Avg","Tsky_1_Avg",
+    "Tair_1_Avg","Tair_2_Avg","RH_1_Avg","RH_2_Avg","VP_1_Avg",
+    "VP_2_Avg","VPsat_1_Avg","VPsat_2_Avg","VPD_1_Avg","VPD_2_Avg",
+    "WS_1_Avg","WDrs_1_Avg","P_1","Psl_1","Tsoil_1_Avg",
+    "SHFsrf_1_Avg","SM_1_Avg","SM_2_Avg","SM_3_Avg",
+    "Tsoil_2","Tsoil_3","Tsoil_4","RF_1_Tot300s","RFint_1_Max"
+  ];
+
+  filteredVariables: string[] = [...this.variableList];
+  variableSearch = '';
+  varDropdownOpen = false;
+
+  filterVariables() {
+    const q = this.variableSearch.toLowerCase();
+    this.filteredVariables = this.variableList.filter(v =>
+      v.toLowerCase().includes(q)
+    );
+  }
+
 
   loadStations() {
     this.http.get(this.stationCsvUrl, { responseType: 'text' })
@@ -76,15 +107,37 @@ export class ErrorReportingComponent {
   }
 
   submitForm() {
-    this.errorService.add(this.formData);
-    alert('Submitted. CSV available at /records.csv');
+    this.submitToGoogleSheet(this.formData);
+
     this.formData = {
       username: '',
       stationNumber: '',
-      variableId: '',
+      variableId: [] as string[],
       startDate: '',
       endDate: '',
       notes: ''
     };
   }
+  toggleVariable(v: string) {
+    if (this.formData.variableId.includes(v)) {
+      this.formData.variableId = this.formData.variableId.filter(x => x !== v);
+    } else {
+      this.formData.variableId.push(v);
+    }
+  }
+
+
+  submitToGoogleSheet(record: any) {
+    const url = 'https://script.google.com/macros/s/AKfycbzT1GnXos5ahoeJAyDTBx73NmntjIovI49UbYgs_HM9RUK0cPpxaNIHUosD40EE9U6Q/exec';
+
+    return fetch(url, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(record)
+    });
+  }
+
 }
